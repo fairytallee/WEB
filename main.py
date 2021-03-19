ec91127a36a537eacc41dfcdd5e66937d4ba4f4d
fairytallee <bryilyant16@gmail.com>
2021-03-19 18:12:56 +0300

    version 1.2

--- main.py
+++ main.py
@@ -43,6 +43,23 @@ def image_mars():
                     </html>"""
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Колонизация</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.jpg')}"
+                        alt="здесь должна была быть картинка, но не нашлась">
+                      </body>
+                    </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
 
