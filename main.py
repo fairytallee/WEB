517ff4630821d99fac0b1f9cc0ffe3ec6f97a5c6
fairytallee <bryilyant16@gmail.com>
2021-03-19 18:40:44 +0300

    version 1.3.2

--- main.py
+++ main.py
@@ -55,8 +55,7 @@ def promotion_image():
                         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
                         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
                         crossorigin="anonymous">
-                        
-                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
                         <title>Колонизация</title>
                       </head>
                       <body>
