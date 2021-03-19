8442da8a8bf747ab5699899e451d1bc0196b5348
fairytallee <bryilyant16@gmail.com>
2021-03-19 18:06:03 +0300

    version 1.0.1

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 from flask import Flask
+from flask import url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/bootstrap_sample')
-def bootstrap():
-    return '''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <title>Привет, Яндекс!</title>
-                  </head>
-                  <body>
-                    <h1>Привет, Яндекс!</h1>
-                    <div class="alert alert-primary" role="alert">
-                      А мы тут компонентами Bootstrap балуемся
-                    </div>
-                  </body>
-                </html>'''
+def first():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    return """Человечество вырастает из детства.<br>
+
+                Человечеству мала одна планета.<br>
+
+                Мы сделаем обитаемыми безжизненные пока планеты.<br>
+
+                И начнем с Марса!<br>
+
+                Присоединяйся!"""
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.jpg')}"
+                        alt="здесь должна была быть картинка, но не нашлась">
+                      </body>
+                    </html>"""
 
 
 if __name__ == '__main__':
