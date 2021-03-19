1330756d24d1b10872a27184a0db90f7ca417bb9
fairytallee <bryilyant16@gmail.com>
2021-03-19 18:16:42 +0300

    version 1.3

--- main.py
+++ main.py
@@ -45,10 +45,17 @@ def image_mars():
 
 @app.route('/promotion_image')
 def promotion_image():
-    return f"""<!doctype html>
+    return f'''<!doctype html>
                     <html lang="en">
                       <head>
                         <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        
                         <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                         <title>Колонизация</title>
                       </head>
@@ -56,8 +63,12 @@ def promotion_image():
                         <h1>Жди нас, Марс!</h1>
                         <img src="{url_for('static', filename='img/mars.jpg')}"
                         alt="здесь должна была быть картинка, но не нашлась">
+                        
+                        <div class="alert alert-primary" role="alert">
+                            А мы тут компонентами Bootstrap балуемся
+                        </div>
                       </body>
-                    </html>"""
+                    </html>'''
 
 
 if __name__ == '__main__':
