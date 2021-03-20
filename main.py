90caf9fedef8c4677772a537ab2254a69248a474
fairytallee <bryilyant16@gmail.com>
2021-03-20 10:34:58 +0300

    version 1.3.5

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def promotion_image():
                         <title>Колонизация</title>
                       </head>
                       <body>
-                        <h1>Жди нас, Марс!</h1>
+                        <h1 style="color: red; text-align: left;">Жди нас, Марс!</h1>
                         <img src="{url_for('static', filename='img/mars.jpg')}"
                         alt="здесь должна была быть картинка, но не нашлась">
                         
@@ -91,13 +91,12 @@ def promotion_image():
                                 </div>
                             </li>
                         </ul>
-
                       </body>
                     </html>'''
 
 
-@app.route('/form_sample', methods=['POST', 'GET'])
-def form_sample():
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def astronaut_selection():
     if request.method == 'GET':
         return f'''<!doctype html>
                         <html lang="en">
@@ -109,52 +108,106 @@ def form_sample():
                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                             crossorigin="anonymous">
                             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Пример формы</title>
+                            <title>Отбор астронавтов</title>
                           </head>
                           <body>
-                            <h1>Форма для регистрации в суперсекретной системе</h1>
+                            <h1>Анкета претендента</h1>
                             <div>
                                 <form class="login_form" method="post">
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
-                                    <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
-                                    <div class="form-group">
-                                        <label for="classSelect">В каком вы классе</label>
-                                        <select class="form-control" id="classSelect" name="class">
-                                          <option>7</option>
-                                          <option>8</option>
-                                          <option>9</option>
-                                          <option>10</option>
-                                          <option>11</option>
-                                        </select>
-                                     </div>
-                                    <div class="form-group">
-                                        <label for="about">Немного о себе</label>
-                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="form-check">Укажите пол</label>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
-                                          <label class="form-check-label" for="male">
-                                            Мужской
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
-                                          <label class="form-check-label" for="female">
-                                            Женский
-                                          </label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        <label class="form-check-label" for="acceptRules">Готов быть добровольцем</label>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Записаться</button>
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <input type="text" class="form-control" id="text" placeholder="Введите фамилию" name="text">
+                                        </li>
+                                        <li>
+                                            <input type="text" class="form-control" id="text" placeholder="Введите имя" name="text">
+                                        </li>
+                                        <li>
+                                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                                        </li>
+                                        <li>
+                                            <div class="form-group">
+                                                <label for="classSelect">Образование</label>
+                                                    <select class="form-control" id="classSelect" name="class">
+                                                        <option>начальное</option>
+                                                        <option>среднее</option>
+                                                        <option>высшее</option>
+                                                    </select>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="form-group">
+                                                <label for="form-check">Предполагаемая должность</label>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="prof" id="ing" value="1" checked>
+                                                    <label class="form-check-label" for="ing">
+                                                        инженер-исследователь
+                                                    </label>
+                                                </div>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="prof" id="pil" value="2">
+                                                    <label class="form-check-label" for="pil">
+                                                        пилот
+                                                    </label>
+                                                </div>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="prof" id="bil" value="3">
+                                                    <label class="form-check-label" for="bil">
+                                                        строитель
+                                                    </label>
+                                                </div>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="prof" id="doc" value="4">
+                                                    <label class="form-check-label" for="doc">
+                                                        врач
+                                                    </label>
+                                                </div>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="prof" id="cyb" value="5">
+                                                    <label class="form-check-label" for="cyb">
+                                                        киберинженер
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="form-group">
+                                                <label for="form-check">Укажите пол</label>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                                    <label class="form-check-label" for="male">
+                                                        Мужской
+                                                    </label>
+                                                </div>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                                    <label class="form-check-label" for="female">
+                                                        Женский
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="form-group">
+                                                <label for="about">Почему вы хотите участвовать?</label>
+                                                <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="form-group">
+                                                <label for="photo">Приложите вашу фотографию</label>
+                                                <input type="file" class="form-control-file" id="photo" name="file">
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="form-group form-check">
+                                                <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                                <label class="form-check-label" for="acceptRules">Политика конфиденциальности</label>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <button type="submit" class="btn btn-primary">Записаться</button>
+                                        </li>
+                                    </ul>
                                 </form>
                             </div>
                           </body>
@@ -168,6 +221,53 @@ def form_sample():
         print(request.form['accept'])
         print(request.form['sex'])
         return "Форма отправлена"
+
+
+@app.route('/choice/<planet_name>')
+def greeting(planet_name):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Варианты выбора: {planet_name}</title>
+                  </head>
+                  <body>
+                    <h1 style="text-align: center; margin-top: 30px; margin-bottom: 30px;">Моё предложение: {planet_name}!</h1>
+                    
+                    <ul class="list-unstyled" style="text-align: center;">
+                            <li>
+                                <div class="alert alert-success" role="alert" color="green">
+                                    <h3>Эта планета недалека от земли</h3>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="alert alert-danger" role="alert">
+                                    <h3>На ней много ресурсов</h3>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="alert alert-info" role="alert">
+                                    <h3>На планете {planet_name} можно терраформировать воду и атмосферу</h3>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="alert alert-warning" role="alert">
+                                    <h3>У планеты {planet_name} есть сильное магнитное поле!</h3>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="alert alert-secondary" role="alert">
+                                    <h3>А также {planet_name} очень красивая планета!</h3>
+                                </div>
+                            </li>
+                        </ul>
+                  </body>
+                </html>'''
 
 
 if __name__ == '__main__':
