94c3d69abf5867d66f6a78ebc3e7e9a3ed25741d
fairytallee <bryilyant16@gmail.com>
2021-04-02 18:04:50 +0300

    version 1.5

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof.lower())
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    profs = ['Инженер', 'Строитель', 'Врач', 'Пилот', 'Ленин', 'Гастроинтеролог', 'Повар', 'Картограф', 'Спецагент',
+             'Юрий Дудь', 'Метеоролог', 'Киберинжинер', 'Пилот дронов', 'Климатолог', 'Астрогеолог',
+             'Петров', 'экзобиолог', 'Денис Тарков']
+    return render_template('list_prof.html', list=list, profs=profs)
+
+
 @app.route('/odd_even')
 def odd_even():
     return render_template('odd_even.html', number=43324)
