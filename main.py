6850ba876f9e7d65d6f66a04e091764f485ebb9c
fairytallee <bryilyant16@gmail.com>
2021-04-03 22:59:17 +0300

    version 1.8.3

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from flask import url_for
 from flask import request
 from PIL import Image
 from io import BytesIO
+from loginform import AstroLoginForm
 from flask import render_template
+from flask import redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/index/<title>')
@@ -33,6 +36,14 @@ def auto_answer():
             'sex': 'мужской', 'motivation': 'Всегда мечтал умереть!', 'ready': 'ДА!'}
     print(list(args.values()))
     return render_template('auto_answer.html', keys=list(args.keys()), elems=list(args.values()))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = AstroLoginForm
+    # if form.validate_on_submit():
+    #     return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
 
 
 @app.route('/odd_even')
